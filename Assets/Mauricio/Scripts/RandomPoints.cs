b17a137331d419d006377a67970b07889628726b
Mauricio <94552142+MMau55@users.noreply.github.com>
2023-05-26 13:00:00 -0500

    Tiempo cada cierto puntaje

--- Assets/Mauricio/Scripts/RandomPoints.cs
+++ Assets/Mauricio/Scripts/RandomPoints.cs
@@ -4,24 +4,57 @@ using UnityEngine;
 
 public class RandomPoints : MonoBehaviour
 {
-    public GameObject Item; // Lista de objetos para generar
+    public GameObject itemPrefab; // Prefab del objeto a generar
+    public Transform spawnPosition; // Posición de generación
+    public GameObject player; // Objeto del personaje
 
-    public float spawnInterval = 1f; // Intervalo de generación
-    public Vector2 spawnPositionRange; // Rango de posición de generación
+    private int pointsThreshold = 10; // Umbral de puntos para generar el objeto
+    private float yOffset = 2f; // Desplazamiento en el eje Y
+
+    private GameObject spawnedObject; // Referencia al objeto generado
 
     private void Start()
     {
-        // Iniciar la generación de objetos en intervalos regulares
-        InvokeRepeating("SpawnRandomObject", spawnInterval, spawnInterval);
+        // Suscribirse al evento OnScoreChanged del ScoreManager
+        ScoreManager.OnScoreChanged += CheckScore;
     }
 
-    private void SpawnRandomObject()
+    private void CheckScore(int score)
     {
-        // Calcular una posición aleatoria dentro del rango establecido
-        float randomX = UnityEngine.Random.Range(spawnPositionRange.x, spawnPositionRange.y);
-        Vector3 spawnPosition = new Vector3(randomX, transform.position.y, transform.position.z);
+        if (score % pointsThreshold == 0)
+        {
+            if (spawnedObject == null)
+            {
+                // Generar una instancia del objeto en la posición establecida
+                spawnedObject = Instantiate(itemPrefab, spawnPosition.position, Quaternion.identity);
 
-        // Instanciar el objeto seleccionado en la posición calculada
-        Instantiate(Item, spawnPosition, Quaternion.identity);
+                // Establecer el objeto generado como hijo del jugador
+                spawnedObject.transform.SetParent(player.transform);
+
+                // Ajustar la posición local del objeto generado
+                Vector3 localPosition = spawnedObject.transform.localPosition;
+                localPosition.y += yOffset;
+                spawnedObject.transform.localPosition = localPosition;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (spawnedObject != null)
+        {
+            // Detectar si se presiona la tecla Espacio
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // Desvincular el objeto generado del jugador
+                spawnedObject.transform.SetParent(null);
+                spawnedObject = null;
+            }
+        }
     }
 }
+
+
+
+
+
