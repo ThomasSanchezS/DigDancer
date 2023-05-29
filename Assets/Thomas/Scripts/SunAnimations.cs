4233f19beb24d60e3c2724081d55d47a0074c986
nekotom2 <thomas.sanchezs@utadeo.edu.co>
2023-05-29 00:46:09 -0500

    Update SunAnimations.cs

--- Assets/Thomas/Scripts/SunAnimations.cs
+++ Assets/Thomas/Scripts/SunAnimations.cs
@@ -9,24 +9,25 @@ public class SunAnimations : MonoBehaviour
     public Transform tittleImage;
     
     public void OnEnable() {
+        Debug.Log("se activo");
         transform.DOMove(new Vector2(450, 350), duration).OnComplete(()=> {tittleImage.transform.DOScale(new Vector2(25,40), duration);});
     }
 
     void OnDisable()
     {
+        Debug.Log("se desactivo");
         StopDeformations();
         ResetDeformations();
     }
 
     void StopDeformations()
     {
-        
         DOTween.Kill(transform);
     }
 
     void ResetDeformations()
     {
-        transform.DOMove(new Vector2(-450, -350), duration);
-        transform.DOKill();
+        transform.DOMove(new Vector2(450, 700), duration).OnComplete(()=> {tittleImage.transform.DOScale(new Vector2(0,0), duration);});;
+        
     }
 }
