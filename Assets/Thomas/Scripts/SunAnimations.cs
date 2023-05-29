82643988b00c5b6b36adaa6db8513f80d36dd806
nekotom2 <thomas.sanchezs@utadeo.edu.co>
2023-05-29 00:07:25 -0500

    avances

--- Assets/Thomas/Scripts/SunAnimations.cs
+++ Assets/Thomas/Scripts/SunAnimations.cs
@@ -7,9 +7,26 @@ public class SunAnimations : MonoBehaviour
 {
     [SerializeField] private float duration = 2;
     public Transform tittleImage;
-    // Start is called before the first frame update
-    void Start()
-    {
+    
+    public void OnEnable() {
         transform.DOMove(new Vector2(450, 350), duration).OnComplete(()=> {tittleImage.transform.DOScale(new Vector2(25,40), duration);});
+    }
+
+    void OnDisable()
+    {
+        StopDeformations();
+        ResetDeformations();
+    }
+
+    void StopDeformations()
+    {
+        
+        DOTween.Kill(transform);
+    }
+
+    void ResetDeformations()
+    {
+        transform.DOMove(new Vector2(-450, -350), duration);
+        transform.DOKill();
     }
 }
